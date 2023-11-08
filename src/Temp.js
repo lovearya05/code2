dcc67e44c42af23677f65d21fb26a8e1a09141d4
lovearya <lovepreetarya1405@gmail.com>
2023-11-08 21:53:33 +0530

    send email functionality implemented

--- src/Temp.js
+++ src/Temp.js
@@ -1,23 +1,57 @@
 import { collection, getDoc, getDocs } from 'firebase/firestore'
 import React, { useEffect } from 'react'
 import { db } from './firebaseConfig'
+import emailjs from 'emailjs-com';
 
 function Temp() {
+  useEffect(() => {
+    sendEmail()
+  }, [])
 
-  useEffect(()=>{
-    const dataRef = collection(db, 'co2')
-    getDocs(dataRef).then(snapshot=>{
-      snapshot.docs.forEach((doc)=>{
-        console.log({id: doc.id, ...doc.data() })
-      })
-    })
+  const serviceId = 'service_6sbrjtd'
+  const templateId = 'template_6s9rv5a'
+  const userId = 'tswF0bxGlMJzKEnZO' // public key
 
+  const emailData = {
+    from_code: 123456,
+    message: 'Hello, this is the email message!',
+    to_email: 'happyarya1405@gmail.com'
+  };
+  const form = document.createElement('form');
+  form.style.display = 'none';
+  for (const key in emailData) {
+    if (emailData.hasOwnProperty(key)) {
+      const input = document.createElement('input');
+      input.type = 'hidden';
+      input.name = key;
+      input.value = emailData[key];
+      form.appendChild(input);
+    }
+  }
 
-  },[])
+  function sendEmail() {
+    document.body.appendChild(form);
+    emailjs.sendForm(serviceId, templateId, form, userId)
+      .then((result) => {
+        console.log(result.text);
+      }, (error) => {
+        console.log(error.text);
+      });
+    document.body.removeChild(form);
+  }
+
+  // useEffect(()=>{
+  //   const dataRef = collection(db, 'co2')
+  //   getDocs(dataRef).then(snapshot=>{
+  //     snapshot.docs.forEach((doc)=>{
+  //       console.log({id: doc.id, ...doc.data() })
+  //     })
+  //   })
+  // },[])
 
   return (
     <div>
-      
+
     </div>
   )
 }
--- src/components/routes/index.js
+++ src/components/routes/index.js
@@ -4,6 +4,7 @@ import Splash from "../login/Splash";
 import Login from "../login/Login";
 import Signup from "../Signup/Signup";
 import Profile from "../Profile/Profile";
+import Temp from "../../Temp";
 
 export default function RoutesServer() {
   return (
@@ -12,7 +13,7 @@ export default function RoutesServer() {
       <Route exact path="/login" element={<Login />}></Route>
       <Route exact path="/signup" element={<Signup />}></Route>
       <Route exact path="/" element={<Profile />}></Route>
-      {/* <Route exact path='/signup' element={< Signup />}></Route> */}
+      <Route exact path='/temp' element={< Temp />}></Route>
     </Routes>
   );
 }
