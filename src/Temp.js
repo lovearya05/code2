ab42befe8abc5486d3299c1f7367e20370ec8fc4
lovearya <lovepreetarya1405@gmail.com>
2023-11-08 22:23:36 +0530

    generate OTP and send email functionality implemented

--- src/Temp.js
+++ src/Temp.js
@@ -1,35 +1,41 @@
 import { collection, getDoc, getDocs } from 'firebase/firestore'
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { db } from './firebaseConfig'
 import emailjs from 'emailjs-com';
 
 function Temp() {
+  const [otp, setOTP] = useState('')
+  const [email, setEmail] = useState('happyarya1405@gmail.com') /////////
   useEffect(() => {
-    sendEmail()
+    // sendEmail()
   }, [])
 
   const serviceId = 'service_6sbrjtd'
   const templateId = 'template_6s9rv5a'
   const userId = 'tswF0bxGlMJzKEnZO' // public key
 
-  const emailData = {
-    from_code: 123456,
-    message: 'Hello, this is the email message!',
-    to_email: 'happyarya1405@gmail.com'
-  };
-  const form = document.createElement('form');
-  form.style.display = 'none';
-  for (const key in emailData) {
-    if (emailData.hasOwnProperty(key)) {
-      const input = document.createElement('input');
-      input.type = 'hidden';
-      input.name = key;
-      input.value = emailData[key];
-      form.appendChild(input);
-    }
-  }
-
   function sendEmail() {
+    const otp = parseInt(Math.random() * 1000000)
+    setOTP(otp)
+    console.log(otp)
+
+    const emailData = {
+      from_code: otp,
+      message: 'Thank You for Registration!',
+      to_email: email
+    };
+
+    const form = document.createElement('form');
+    form.style.display = 'none';
+    for (const key in emailData) {
+      if (emailData.hasOwnProperty(key)) {
+        const input = document.createElement('input');
+        input.type = 'hidden';
+        input.name = key;
+        input.value = emailData[key];
+        form.appendChild(input);
+      }
+    }
     document.body.appendChild(form);
     emailjs.sendForm(serviceId, templateId, form, userId)
       .then((result) => {
@@ -40,14 +46,18 @@ function Temp() {
     document.body.removeChild(form);
   }
 
+
   // useEffect(()=>{
-  //   const dataRef = collection(db, 'co2')
-  //   getDocs(dataRef).then(snapshot=>{
-  //     snapshot.docs.forEach((doc)=>{
-  //       console.log({id: doc.id, ...doc.data() })
-  //     })
-  //   })
+  // fetchDataFromFirebase()
   // },[])
+  const fetchDataFromFirebase = () => {
+    const dataRef = collection(db, 'co2')
+    getDocs(dataRef).then(snapshot => {
+      snapshot.docs.forEach((doc) => {
+        console.log({ id: doc.id, ...doc.data() })
+      })
+    })
+  }
 
   return (
     <div>
