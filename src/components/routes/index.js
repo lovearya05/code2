9b5452938cbbb06b8dd4ecffd9d68af6cac9473c
lovearya <lovepreetarya1405@gmail.com>
2023-11-09 09:17:34 +0530

    gh pages deploy

--- src/components/routes/index.js
+++ src/components/routes/index.js
@@ -8,19 +8,19 @@ import OtpScreen from "../OtpScreen/OtpScreen";
 import Temp from "../../Temp";
 
 export default function RoutesServer() {
-    
+
     return (
         <div style={{ backgroundColor: "#272727", width: "100%" }}>
             <div
                 style={{ display: "flex", flexDirection: "column", minHeight: "100vh" }}
             >
                 <Routes>
-                    <Route exact path="/" element={<Splash />}></Route>
-                    <Route exact path="/login" element={<Login />}></Route>
-                    <Route exact path="/signup" element={<Signup />}></Route>
-                    <Route exact path="/profile" element={<Profile />}></Route>
+                    <Route exact path="/code2/" element={<Splash />}></Route>
+                    <Route exact path="/code2/login" element={<Login />}></Route>
+                    <Route exact path="/code2/signup" element={<Signup />}></Route>
+                    <Route exact path="/code2/profile" element={<Profile />}></Route>
                     {/* <Route exact path="/otp" element={<OtpScreen />}></Route> */}
-                    <Route exact path='/temp' element={< Temp />}></Route>
+                    <Route exact path='/code2/temp' element={< Temp />}></Route>
                     {/* <Route exact path='/signup' element={< Signup />}></Route> */}
                 </Routes>
             </div>
